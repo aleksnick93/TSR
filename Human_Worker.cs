257d26f032fbd1c9b3470c76fec62288498ed010
Alex Nick <aleksnick93@yandex.ru>
2015-08-20 10:49:43 +0300

    Начало работы с классом опреляющим эффективность офисного работника

---
+++ Human_Worker.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TestProj
+{
+	public class Human_Worker
+	{
+		int success;
+		int motivation;
+		int talent;
+		int skill;
+		bool isSuccessWorker = true;
+		
+		
+		
+	}
+
+}
